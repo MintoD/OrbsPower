7c4eeb017d6861f55e17f434aa782b9d916c14ee
MintoD <mintothedev@protonmail.com>
2021-05-04 18:51:07 +0700

    Add some comment

--- src/dev/Orb.ts
+++ src/dev/Orb.ts
@@ -1,10 +1,28 @@
+/* 
+
+   ____       _         _____                       
+  / __ \     | |       |  __ \                      
+ | |  | |_ __| |__  ___| |__) |____      _____ _ __ 
+ | |  | | '__| '_ \/ __|  ___/ _ \ \ /\ / / _ \ '__|
+ | |__| | |  | |_) \__ \ |  | (_) \ V  V /  __/ |   
+  \____/|_|  |_.__/|___/_|   \___/ \_/\_/ \___|_|   
+                                                    
+                                   Version: 1.1                 
+
+ */
+
 class Orb {
     stringID: string;
     itemName: string;
     itemTexture: Item.TextureData;
     itemConfig: any;
 
-    constructor(stringID: string, itemName: string, itemTexture: Item.TextureData, itemConfig: any) {
+    constructor(
+        stringID: string,
+        itemName: string,
+        itemTexture: Item.TextureData,
+        itemConfig: any
+    ) {
         this.stringID = stringID;
         this.itemName = itemName;
         this.itemTexture = itemTexture;
@@ -13,7 +31,12 @@ class Orb {
 
     createItem(): void {
         IDRegistry.genItemID(this.stringID);
-        Item.createItem(this.stringID, this.itemName, this.itemTexture, this.itemConfig);
+        Item.createItem(
+            this.stringID,
+            this.itemName,
+            this.itemTexture,
+            this.itemConfig
+        );
     }
 
     getID(): number {
