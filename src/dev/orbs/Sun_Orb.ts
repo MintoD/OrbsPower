0f724b9ebbd56e3c26c2fb9d3956e64db7721586
Toby <mintothedev@protonmail.com>
2021-05-06 11:46:00 +0700

    Update Sun_Orb.ts

--- src/dev/orbs/Sun_Orb.ts
+++ src/dev/orbs/Sun_Orb.ts
@@ -39,8 +39,7 @@ Recipes.addShaped(
 Callback.addCallback("ItemUse", (coords, item, block) => {
     if (item.id == sun_orb.getID()) {
         for (let i = 0; i < 36; i++) {
-            if (Player.getInventorySlot(i).id == 289) {
-                if (Player.getInventorySlot(i).count > 1) {
+            if (Player.getInventorySlot(i).id == 289 && Player.getInventorySlot(i).count > 1) {
                     Player.setInventorySlot(
                         i,
                         289,
@@ -49,7 +48,7 @@ Callback.addCallback("ItemUse", (coords, item, block) => {
                     );
                     World.setWorldTime(9000);
                     Game.tipMessage(Native.Color.GREEN + "Success!");
-                }
+   
             }
         }
     }
